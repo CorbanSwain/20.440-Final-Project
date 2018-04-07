7d1a47484558e780aceb1c3d6757d4f5a63f091e
CorbanSwain <CorbanSwain@gmail.com>
2018-04-07 10:42:37 -0400

    Added properties to Datased class, and implemented slicing for normal and tumor data

--- tanric_processing.py
+++ tanric_processing.py
@@ -17,34 +17,31 @@ def file2dict(file, delimiter='\t'):
         value = row[1]
         try:
             value = int(value)
-        except ValueError:
+        except ValueError:  # can't cast to int, try float
             try:
                 value = float(value)
-            except ValueError:
+            except ValueError:  # can't cast to float, keep as string
                 pass
         metadict[key] = value
     return metadict
 
 
-m_dict = file2dict('data/tanric_data/TCGA-BLCA-rnaexpr-META.tsv')
-
-data = np.genfromtxt('data/tanric_data/TCGA-BLCA-rnaexpr.tsv', delimiter='\t',
-                  dtype=None, encoding=None, names=True, deletechars='')
-
-
 class Dataset:
     def __init__(self, metadict, exprdata=None):
         self.metadict = metadict
-        self.n_normal_samples = metadict['Num_Normal_Samples']
-        self.n_tumor_samples = metadict['Num_Tumor_Samples']
         self.cancer_type = metadict['Cancer_Type']
-        self.gene_ids = None
-        self.n_genes = None
-        self.sample_names = None
-        self.values = None
-
+        self.n_normal_samples = metadict['Num_Normal_Samples']
+        self.normal_sel = np.arange(self.n_normal_samples)
+        self.n_tumor_samples = metadict['Num_Tumor_Samples']
+        self.n_samples = self.n_normal_samples + self.n_tumor_samples
+        self.tumor_sel = np.arange(self.n_normal_samples, self.n_samples)
         if exprdata is not None:
             self.parse_exprdata(exprdata)
+        else:
+            self.gene_ids = None
+            self.n_genes = None
+            self.sample_names = None
+            self.values = None
 
     def parse_exprdata(self, exprdata):
         self.gene_ids = [s.strip('\"') for s in exprdata['Gene_ID']]
@@ -53,5 +50,23 @@ class Dataset:
         self.values = exprdata[self.sample_names]
         self.values = self.values.view(np.float64).reshape((self.n_genes, -1))
 
+        if (self.n_samples - self.values.shape[1]) < 1:
+            raise ValueError('Metadata value for n_samples (%d) is not '
+                             'equal to the number of columns found in the '
+                             'expression data file (%d).'
+                             % (self.n_samples, self.values.shape[1]))
 
+    @property
+    def normal_samples(self):
+        return self.values[:, self.normal_sel]
+
+    @property
+    def tumor_samples(self):
+        return self.values[:, self.tumor_sel]
+
+
+
+m_dict = file2dict('data/tanric_data/TCGA-BLCA-rnaexpr-META.tsv')
+data = np.genfromtxt('data/tanric_data/TCGA-BLCA-rnaexpr.tsv', delimiter='\t',
+                  dtype=None, encoding=None, names=True, deletechars='')
 dataset = Dataset(m_dict, data)
