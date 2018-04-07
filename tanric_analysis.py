977b30b06aafd2fa92c215b88f82faa192a86753
Corban Swain <CorbanSwain@gmail.com>
2018-04-07 14:04:23 -0400

    Adding t-test and multiple hypotehsis testing methods

--- tanric_analysis.py
+++ tanric_analysis.py
@@ -1,10 +1,12 @@
 #!/bin/python3
-# tanric_utils.py
+# utils.py
 # Corban Swain, 2018
 
 import os
 import numpy as np
-from tanric_utils import file2dict, TanricDataset
+import matplotlib.pyplot as plt
+from utils import *
+from scipy.stats import ttest_ind
 
 MIN_NORMAL_SAMPLES = 5
 
@@ -18,7 +20,7 @@ def import_all_data():
                 names.append(line.strip(' \n'))
 
     datasets = []
-    for name in names: # TODO - this can be parallelized
+    for name in names:  # TODO - this can be parallelized
         metafile = os.path.join(tanric_dir, name + '-rnaexpr-META.tsv')
         tds = TanricDataset(file2dict(metafile))
 
@@ -39,15 +41,26 @@ def import_all_data():
         print('\tLoaded in dataset for %s (%3d, %3d)'
               % (tds.cancer_type, tds.n_normal_samples, tds.n_tumor_samples))
         datasets.append(tds)
-
     return datasets
 
 
+def perform_t_test(datasets, expr_cutoff=0.3, procedure='crit', **kwargs):
+    # FIXME - May need to do some memory management here
+    find_signif = mh_tests[procedure]
+    for ds in datasets:
+        print('\t' + ds.cancer_type)
+        valid = np.any(ds.exprdata > expr_cutoff, 1)
+        norm_valid = ds.normal_samples[valid]
+        tumor_valid = ds.tumor_samples[valid]
+        t, p = ttest_ind(norm_valid, tumor_valid, axis=1)
+        signif = find_signif(p, **kwargs)
+        print('\t\tnum signif = %d' % len(signif))
 
 
 if __name__ == "__main__":
-    print('Beginning Data Import')
+    print('1-Beginning Data Import')
     datasets = import_all_data()
 
-    print('Performing t-test')
+    print('2-Performing t-tests')
+    perform_t_test(datasets, procedure='bonferoni')
 
---
+++ utils.py
@@ -0,0 +1,102 @@
+#!/bin/python3
+# utils.py
+# Corban Swain, 2018
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import rankdata
+import warnings
+
+
+def structarr2nparr(x):
+    # TODO - check for max precision
+    with warnings.catch_warnings():
+        # Ignoring warning about viewing an array returned by selecting
+        # multiple fields in a structured array.
+        # see: https://github.com/numpy/numpy/issues/8383
+        warnings.simplefilter('ignore')
+        return x.view(np.float64).reshape((len(x), -1))
+
+
+def file2dict(file, delimiter='\t'):
+    data = np.genfromtxt(file, dtype=None, delimiter=delimiter, encoding=None)
+    metadict = {}
+    for row in data:
+        key = row[0]
+        value = row[1]
+        try:
+            value = int(value)
+        except ValueError:  # can't cast to int, try float
+            try:
+                value = float(value)
+            except ValueError:  # can't cast to float, keep as string
+                pass
+        metadict[key] = value
+    return metadict
+
+
+# benjamani-hochberg procedure for
+def argbenhoch(p, q=0.1, plot=False):
+    i = rankdata(p, method='ordinal')
+    m = len(p)
+    bh_crit = q * i / m
+    try:
+        cutoff_i = np.max(i[np.where(p <= bh_crit)[0]])
+    except ValueError:
+        return np.array([])
+    signif_idx = np.where(i <= cutoff_i)[0]
+
+    if plot:
+        s = np.argsort(i)
+        plt.plot(i[s], bh_crit[s],  i[s], p[s])
+        plt.plot(i[signif_idx], p[signif_idx], 'r.')
+        plt.xlabel('Rank')
+        plt.ylabel('P Value')
+        plt.xlim([0, m])
+        plt.ylim([0, 1])
+        plt.show()
+
+    return signif_idx[np.argsort(p[signif_idx])]
+
+
+mh_tests = {'ben-hoch': argbenhoch,
+            'crit': lambda p, a=0.05: np.where(p < a)[0],
+            'bonferoni': lambda p, a=0.05: np.where(p < (a / len(p)))[0]}
+
+
+class TanricDataset:
+    def __init__(self, metadict, expr_structarr=None):
+        self.metadict = metadict
+        self.cancer_type = metadict['Cancer_Type']
+        self.n_normal_samples = metadict['Num_Normal_Samples']
+        self.normal_sel = np.arange(self.n_normal_samples)
+        self.n_tumor_samples = metadict['Num_Tumor_Samples']
+        self.n_samples = self.n_normal_samples + self.n_tumor_samples
+        self.tumor_sel = np.arange(self.n_normal_samples, self.n_samples)
+        self.results = {}
+        if expr_structarr is not None:
+            self.parse_exprdata(expr_structarr)
+        else:
+            self.gene_ids = None
+            self.n_genes = None
+            self.sample_names = None
+            self.exprdata = None
+
+    def parse_exprdata(self, expr_structarr):
+        self.gene_ids = [s.strip('\"') for s in expr_structarr['Gene_ID']]
+        self.n_genes = len(self.gene_ids)
+        self.sample_names = list(expr_structarr.dtype.names[1:])
+        self.exprdata = structarr2nparr(expr_structarr[self.sample_names])
+        if not (self.n_samples - self.exprdata.shape[1]) < 1:
+            raise ValueError('Metadata value for n_samples (%d) is not '
+                             'equal to the number of columns found in the '
+                             'expression data file (%d).'
+                             % (self.n_samples, self.exprdata.shape[1]))
+
+    @property
+    def normal_samples(self):
+        return self.exprdata[:, self.normal_sel]
+
+    @property
+    def tumor_samples(self):
+        return self.exprdata[:, self.tumor_sel]
