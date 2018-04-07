729e4d65c48ac2f8fe38fb61bf6c5fd60478a3b1
Corban Swain <CorbanSwain@gmail.com>
2018-04-07 00:57:39 -0400

    working data import and Dataset class

--- tanric_processing.py
+++ tanric_processing.py
@@ -5,7 +5,6 @@
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import stats
 
 
 def file2dict(file, delimiter='\t'):
@@ -27,10 +26,32 @@ def file2dict(file, delimiter='\t'):
     return metadict
 
 
-d = file2dict('data/tanric_data/TCGA-BLCA-rnaexpr-META.tsv')
+m_dict = file2dict('data/tanric_data/TCGA-BLCA-rnaexpr-META.tsv')
+
+data = np.genfromtxt('data/tanric_data/TCGA-BLCA-rnaexpr.tsv', delimiter='\t',
+                  dtype=None, encoding=None, names=True, deletechars='')
 
 
 class Dataset:
-    def __init__(self, metadict, exprdata):
-        pass
+    def __init__(self, metadict, exprdata=None):
+        self.metadict = metadict
+        self.n_normal_samples = metadict['Num_Normal_Samples']
+        self.n_tumor_samples = metadict['Num_Tumor_Samples']
+        self.cancer_type = metadict['Cancer_Type']
+        self.gene_ids = None
+        self.n_genes = None
+        self.sample_names = None
+        self.values = None
 
+        if exprdata is not None:
+            self.parse_exprdata(exprdata)
+
+    def parse_exprdata(self, exprdata):
+        self.gene_ids = [s.strip('\"') for s in exprdata['Gene_ID']]
+        self.n_genes = len(self.gene_ids)
+        self.sample_names = list(exprdata.dtype.names[1:])
+        self.values = exprdata[self.sample_names]
+        self.values = self.values.view(np.float64).reshape((self.n_genes, -1))
+
+
+dataset = Dataset(m_dict, data)
